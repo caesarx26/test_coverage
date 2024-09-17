72cf7415440a4aa00d3c71088a5d407d50773526
caesarx26 <caesarrobles26@gmail.com>
2024-09-16 23:04:25 -0700

    adding more tests

--- tests/test_account.py
+++ tests/test_account.py
@@ -48,3 +48,80 @@ def test_create_an_account():
     account.create()
     assert len(Account.all()) == 1
 
+def test_repr():
+    """Test the representation of an account"""
+    account = Account()
+    account.name = "Foo"
+    assert str(account) == "<Account 'Foo'>"
+    
+def test_to_dict():
+    """ Test account to dict """
+    rand = randrange(0, len(ACCOUNT_DATA))  # Generate a random index
+    data = ACCOUNT_DATA[rand]  # get a random account
+    account = Account(**data)
+    result = account.to_dict()
+
+    assert account.name == result["name"]
+    assert account.email == result["email"]
+    assert account.phone_number == result["phone_number"]
+    assert account.disabled == result["disabled"]
+    assert account.date_joined == result["date_joined"]
+    
+def test_from_dict():
+    """ Test Account creation from a dictionary """
+    rand = randrange(0, len(ACCOUNT_DATA))  # Generate a random index
+    data = ACCOUNT_DATA[rand]  # get a random account
+    account = Account()
+    account.from_dict(data)
+    account.create()
+    
+    # Check that attributes were set correctly
+    assert account.name == data.get("name")
+    assert account.email == data.get("email")
+    assert account.phone_number == data.get("phone_number")
+    assert account.disabled == data.get("disabled")
+    
+    
+def test_update():
+    """ Test updating an account"""
+    # Create an account manually 
+    account_data = {
+        "name": "John Test",
+        "email": "john@test.com",
+        "phone_number": "702-234-4456",
+        "disabled": False
+    }
+    account = Account(**account_data)
+    account.create()
+
+    # Update the account
+    account.name = "Updated Account Name"
+    account.update()
+
+    # Verify the account was updated
+    updated_account = Account.find(account.id)
+    assert updated_account.name == "Updated Account Name"
+    
+def test_delete():
+    """ Test deleting an account"""
+    # Create an account manually
+    account_data = account_data = {
+        "name": "John Test",
+        "email": "john@test.com",
+        "phone_number": "702-234-4456",
+        "disabled": False
+    }
+    account = Account(**account_data)
+    account.create()
+    
+    # Ensure account is created
+    assert Account.find(account.id) is not None
+    
+    # Delete the account
+    account.delete()
+    
+    # Verify the account was deleted
+    assert Account.find(account.id) is None
+
+    
+       
